68320e23a1b259d0a27d66f7cc664b332e421fa0
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2021-01-20 11:08:26 -0600

    Add files via upload

---
+++ ComputerTest.java
@@ -0,0 +1,19 @@
+package Encapsulation;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+        Computer computer = new Computer("Apple", "Intel", 999);
+        System.out.println(computer.getBrand());
+        System.out.println(computer.getCPU());
+
+        // computer.setPrice();   Since we did not create the setter inside the Computer class
+        // it is called immutable class
+
+    }
+
+}
+
+
+
+
